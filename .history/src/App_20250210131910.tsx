d6eef62f48baf32f1560e68f80cb3b7249ff8b1d
Giltih <giladbass@gmail.com.com>
2025-02-10 13:20:28 +0200

    fourth push

--- .history/src/App_20250210131910.tsx
+++ .history/src/App_20250210131910.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import './App.css';
 import LotteryInterface from './components/LotteryInterface';
  
-const App: React.FC = () => {
+
+
+  const handleNumberSelect = (tableIndex: number, number: number) => {
+    const newSelectedNumbers = [...selectedNumbers];
+    const tableNumbers = newSelectedNumbers[tableIndex] || [];
+
+    if (tableNumbers.includes(number)) {
+      newSelectedNumbers[tableIndex] = tableNumbers.filter((num) => num !== number);
+    } else if (tableNumbers.length < 6) {
+      newSelectedNumbers[tableIndex] = [...tableNumbers, number];
+    }
+
+    setSelectedNumbers(newSelectedNumbers);
+  };
 
   return (
     <div className="app-container">
--- .history/src/App_20250210131955.tsx
+++ .history/src/App_20250210131955.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import './App.css';
 import LotteryInterface from './components/LotteryInterface';
